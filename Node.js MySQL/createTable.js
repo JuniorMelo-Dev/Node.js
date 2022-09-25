0dfe564df80b57f0a84dd1d87b3217421c8f4f44
JuniorMelo <juniormelo.dev@gmail.com>
2022-09-25 11:51:52 -0300

    Criando tabela e fk_id

---
+++ Node.js MySQL/createTable.js
@@ -0,0 +1,33 @@
+// Criando tabela Clientes e Primary Key
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+      host:"localhost",
+      port: "3306",
+      user: "root",
+      password: "",
+      database: "db_nodejs_mysql"
+});
+
+//Conexão com banco para criar tabela.
+// con.connect(function(err) {
+//       if (err) throw err;
+//       console.log("Connected!");
+
+//       var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+//       con.query(sql, function (err, result) {
+//             if (err) throw err;
+//             console.log("Table created!");
+//       });
+// });
+
+con.connect(function(err) {
+      if (err) throw err;
+      console.log("Connected");
+      var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+      con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table altered");
+      });
+});
